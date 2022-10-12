7bbe030b7b3eed4cb948b74ae71b0f7d4db6f2d0
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2022-10-12 14:20:48 +0800

    restore

--- lightlyshaders.cpp
+++ lightlyshaders.cpp
@@ -193,6 +193,15 @@ LightlyShadersEffect::windowAdded(EffectWindow *w)
             || w->windowClass().contains("ulauncher", Qt::CaseInsensitive)
             || w->windowClass().contains("ksplash", Qt::CaseInsensitive)
             || w->windowClass().contains("ksmserver", Qt::CaseInsensitive)
+            || w->windowClass().contains("netease-cloud-music", Qt::CaseInsensitive) //网易云音乐 桌面歌词
+            || w->windowClass().contains("tim.exe", Qt::CaseInsensitive) // Wine TIM
+            || w->windowClass().contains("wechat.exe", Qt::CaseInsensitive) // Wine 微信
+            || w->windowClass().contains("wechatapp.exe", Qt::CaseInsensitive) // Wine 微信（小程序）
+            || w->windowClass().contains("wemeetapp", Qt::CaseInsensitive) // 腾讯会议
+            || w->windowClass().contains("waydroid", Qt::CaseInsensitive) // WayDroid
+            || w->windowClass().contains("todesk", Qt::CaseInsensitive) // ToDesk
+            || w->windowClass().contains("genshinimpact.exe", Qt::CaseInsensitive) // 原神
+            || w->windowClass().contains("org.jackhuang.hmcl.launcher", Qt::CaseInsensitive) // HMCL
             || (w->windowClass().contains("reaper", Qt::CaseInsensitive) && !hasShadow(w))))
         return;
 
